478a1499a946c4addd54714538cd66ded0a4d780
Verococha <kepler_452b@sjtu.edu.cn>
2024-11-22 08:21:37 +0800

    finish lang.c

--- lang.c
+++ lang.c
@@ -24,6 +24,108 @@ struct frontend_regexp *TFr_Option(struct frontend_regexp *r)
     return fr;
 }
 
+struct frontend_regexp *TFr_Star(struct frontend_regexp *r)
+{
+    struct frontend_regexp *fr;
+    fr = (struct frontend_regexp *)malloc(sizeof(struct frontend_regexp));
+    fr->t = T_FR_STAR;
+    fr->d.STAR.r = r;
+    return fr;
+}
+
+struct frontend_regexp *TFr_Plus(struct frontend_regexp *r)
+{
+    struct frontend_regexp *fr;
+    fr = (struct frontend_regexp *)malloc(sizeof(struct frontend_regexp));
+    fr->t = T_FR_PLUS;
+    fr->d.PLUS.r = r;
+    return fr;
+}
+
+struct frontend_regexp *TFr_String(char *s)
+{
+    struct frontend_regexp *fr;
+    fr = (struct frontend_regexp *)malloc(sizeof(struct frontend_regexp));
+    fr->t = T_FR_STRING;
+    fr->d.STRING.s = s;
+    return fr;
+}
+
+struct frontend_regexp *TFr_SingleChar(char c)
+{
+    struct frontend_regexp *fr;
+    fr = (struct frontend_regexp *)malloc(sizeof(struct frontend_regexp));
+    fr->t = T_FR_SINGLE_CHAR;
+    fr->d.SINGLE_CHAR.c = c;
+    return fr;
+}
+
+struct frontend_regexp *TFr_Union(struct frontend_regexp *r1, struct frontend_regexp *r2)
+{
+    struct frontend_regexp *fr;
+    fr = (struct frontend_regexp *)malloc(sizeof(struct frontend_regexp));
+    fr->t = T_FR_UNION;
+    fr->d.UNION.r1 = r1;
+    fr->d.UNION.r2 = r2;
+    return fr;
+}
+
+struct frontend_regexp *TFr_Concat(struct frontend_regexp *r1, struct frontend_regexp *r2)
+{
+    struct frontend_regexp *fr;
+    fr = (struct frontend_regexp *)malloc(sizeof(struct frontend_regexp));
+    fr->t = T_FR_CONCAT;
+    fr->d.CONCAT.r1 = r1;
+    fr->d.CONCAT.r2 = r2;
+    return fr;
+}
+
+struct simpl_regexp *TS_CharSet(struct char_set *c)
+{
+    struct simpl_regexp *sr;
+    sr = (struct simpl_regexp *)malloc(sizeof(struct simpl_regexp));
+    sr->t = T_S_CHAR_SET;
+    sr->d.CHAR_SET = *c;
+    return sr;
+}
+
+struct simpl_regexp *TS_Star(struct simpl_regexp *r)
+{
+    struct simpl_regexp *sr;
+    sr = (struct simpl_regexp *)malloc(sizeof(struct simpl_regexp));
+    sr->t = T_S_STAR;
+    sr->d.STAR.r = r;
+    return sr;
+}
+
+struct simpl_regexp *TS_EmptyStr()
+{
+    struct simpl_regexp *sr;
+    sr = (struct simpl_regexp *)malloc(sizeof(struct simpl_regexp));
+    sr->t = T_S_EMPTY_STR;
+    return sr;
+}
+
+struct simpl_regexp *TS_Union(struct simpl_regexp *r1, struct simpl_regexp *r2)
+{
+    struct simpl_regexp *sr;
+    sr = (struct simpl_regexp *)malloc(sizeof(struct simpl_regexp));
+    sr->t = T_S_UNION;
+    sr->d.UNION.r1 = r1;
+    sr->d.UNION.r2 = r2;
+    return sr;
+}
+
+struct simpl_regexp *TS_Concat(struct simpl_regexp *r1, struct simpl_regexp *r2)
+{
+    struct simpl_regexp *sr;
+    sr = (struct simpl_regexp *)malloc(sizeof(struct simpl_regexp));
+    sr->t = T_S_CONCAT;
+    sr->d.CONCAT.r1 = r1;
+    sr->d.CONCAT.r2 = r2;
+    return sr;
+}
+
 struct finite_automata *create_empty_graph()
 {
     struct finite_automata *fa;
